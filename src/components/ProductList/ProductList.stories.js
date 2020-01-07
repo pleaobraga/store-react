139686c70dd16357b622a0d0a25e6a497c2fed32
Pedro <pleao.braga@gmail.com>
2020-01-07 20:16:10 -0300

    Improved ProductList CSS

--- src/components/ProductList/ProductList.stories.js
+++ src/components/ProductList/ProductList.stories.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import ProductList from './ProductList'
+import { ProductList } from './ProductList'
 
 const listItems = [
   {
     id: '0',
-    name: 'Name',
+    name: 'Product 1',
     quantity: '1',
     price: '10,00',
+    currency: 'R$'
+  },
+  {
+    id: '1',
+    name: 'Product 2',
+    quantity: '2',
+    price: '20,00',
+    currency: 'R$'
+  },
+  {
+    id: '2',
+    name: 'Product 3',
+    quantity: '3',
+    price: '30,00',
+    currency: 'R$'
+  },
+  {
+    id: '3',
+    name: 'Product 4',
+    quantity: '4',
+    price: '40,00',
     currency: 'R$'
   }
 ]
